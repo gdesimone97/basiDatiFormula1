8a44832b103422908414f839480488074430d758
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-05-16 16:57:29 +0200

    Viste

--- Script/Viste.sql
+++ Script/Viste.sql
@@ -1,24 +1,39 @@
 -- Script per la generazione e l'aggiornamento delle viste principali
 
-create view CLASSIFICA_PILOTI(codice_pilota, punteggio) as 
+
+create or replace function CREAZIONE_VIEW_CLASSIFICA_PILOTI() returns trigger as $$
+begin
+
+	create view CLASSIFICA_PILOTI(codice_pilota, punteggio) as 
 	select codice_pilota, sum(punteggio)
 	from risultati
 	where numero_campionato = NEW.numero_campionato
 	group by codice_pilota
-	order by sum(punteggio) desc
+	order by sum(punteggio) desc;
+	
+return null;
+end $$ language plpgsql;
+
+create or replace function CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI() returns trigger as $$
+begin
 
 create view CLASSIFICA_COSTRUTTORI (nome_scuderia, punteggio) as
+
 	select nome_scuderia, sum(C.punteggio)
 	from CLASSIFICA_PILOTI as C join AFFERENZA_PILOTI as A on (C.codice_pilota = A.codice_pilota)
 	where A.numero_campionato = NEW.numero_campionato
 	group by nome_scuderia
-	order by sum(C.punteggio) desc
-
---fare trigger su campionato insert
---aggiornare trigger di risultati aggiungendo le due query
-
-create or replace function CREAZIONE_VIEW_CLASSIFICA_PILOTI() returns trigger as $$
-begin
+	order by sum(C.punteggio) desc;
 	
 return null;
 end $$ language plpgsql;
+
+create trigger CREAZIONE_VIEW_CLASSIFICA_PILOTI 
+after insert on campionati
+for each statement
+execute procedure CREAZIONE_VIEW_CLASSIFICA_PILOTI();
+
+create trigger CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI
+after insert on campionati
+for each statement
+execute procedure CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI();

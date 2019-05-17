02654eb0aa74da3afe57f08e27d05ecc90046a14
fonzdm <33768748+fonzdm@users.noreply.github.com>
2019-05-17 17:37:18 +0200

    Update CreaDatabase.sql

--- Script/CreaDatabase.sql
+++ Script/CreaDatabase.sql
@@ -130,8 +130,8 @@ create table RISULTATI (
    TEMPO_QUALIFICA      TempoGiro            null,
    RITIRATO             BOOL               	 not null,
    constraint PK_RISULTATI primary key (SEDE_PISTA, NOME_PISTA, CODICE_PILOTA, NUMERO_CAMPIONATO),
-	check(punteggio between 0 and 25),
-	check((punteggio <> 0 and not ritirato) or (punteggio = 0 and ritirato))
+	check(punteggio between 1 and 26),
+	check((punteggio <> 0 and not ritirato) or (punteggio = 0))
 );
 
 /*==============================================================*/

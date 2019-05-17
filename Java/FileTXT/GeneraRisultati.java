4661a86be185bd03eefaddc8c7ab743e961af140
GiackV <1997gennaro@gmail.com>
2019-05-17 13:08:18 +0200

    Accorgimenti

--- Java/FileTXT/GeneraRisultati.java
+++ Java/FileTXT/GeneraRisultati.java
@@ -8,6 +8,7 @@ package popoladatabase;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,53 +24,49 @@ public class GeneraRisultati {
         int num;
         int cont = 0;
         int punteggio;
-        int minuti;
-        int secondi;
-        int millisecondi;
         String tempoms;
-        int tempoqual;
         int inizio;
         String nomeFile = "risultati.txt";
 
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter(":");
 
-        try (BufferedWriter w = new BufferedWriter(new FileWriter(nomeFile))) {
+        System.out.print("Numero Campionato: ");
+        campionato = sc.nextLine();
+        System.out.print("Numero giornate da inserire per questo campionato: ");
+        num = Integer.parseInt(sc.nextLine());
+        System.out.print("Da quale giornata vuoi inserire? ");
+        inizio = Integer.parseInt(sc.nextLine());
 
-            System.out.print("Numero Campionato: ");
-            campionato = sc.nextLine();
-            System.out.print("Numero giornate da inserire per questo campionato: ");
-            num = Integer.parseInt(sc.nextLine());
-            System.out.print("Da quale giornata vuoi inserire? ");
-            inizio = Integer.parseInt(sc.nextLine());
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(nomeFile, true))) {
 
             //PER OGNI PILOTA:
             while (cont < num) {
                 for (int i = 0; i < 20; i++) {
-                    w.write((campionato) + ":");
-                    w.write((cont + inizio) + ":");
+                    out.write((campionato) + ":");
+                    out.write((cont + inizio) + ":");
 
                     System.out.print("Codice Pilota: ");
-                    w.write(sc.nextLine() + ":");
+                    out.write(sc.nextLine() + ":");
 
                     System.out.print("Punteggio: ");
                     punteggio = Integer.parseInt(sc.nextLine());
-                    w.write(punteggio + ":");
+                    out.write(punteggio + ":");
 
                     System.out.print("Miglior tempo (mm:ss:lll): ");
                     tempoms = sc.nextLine();
-                    w.write(tempoms + ":");
+                    out.write(tempoms + ":");
 
                     System.out.print("Tempo Qualifica (mm:ss:lll): ");
-                    w.write(sc.nextLine() + ":");
+                    out.write(sc.nextLine() + ":");
 
                     if (punteggio == 0 & tempoms.compareTo("0:0:0") == 0) {
-                        w.write("1");
+                        out.write("1");
                     } else {
-                        w.write("0");
+                        out.write("0");
                     }
                     System.out.println("PILOTA INSERITO\n");
-                    w.newLine();
+                    out.newLine();
                 }
                 cont++;
             }

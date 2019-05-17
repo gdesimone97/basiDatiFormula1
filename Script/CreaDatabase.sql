0af695737430e6593eec957483f6472960aa27c1
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-05-17 16:48:00 +0200

    Update CreaDatabase.sql

--- Script/CreaDatabase.sql
+++ Script/CreaDatabase.sql
@@ -52,7 +52,8 @@ create table CALENDARIO (
    NUMERO_CAMPIONATO    INT                  not null,
    DATA                 DATE                 not null,
    NUMERO_GIORNATA		INT					 not null,
-   constraint PK_CALENDARIO primary key (SEDE_PISTA, NOME_PISTA, NUMERO_CAMPIONATO)
+   constraint PK_CALENDARIO primary key (SEDE_PISTA, NOME_PISTA, NUMERO_CAMPIONATO),
+   check(NUMERO_GIORNATA between 1 and 21)
 );
 /*==============================================================*/
 /* Table: CAMPIONATI                                            */
@@ -131,7 +132,7 @@ create table RISULTATI (
    TEMPO_QUALIFICA      TempoGiro            null,
    RITIRATO             BOOL               	 not null,
    constraint PK_RISULTATI primary key (SEDE_PISTA, NOME_PISTA, CODICE_PILOTA, NUMERO_CAMPIONATO),
-	check(punteggio between 0 and 25),
+	check(punteggio between 0 and 26),
 	check((punteggio <> 0 and not ritirato) or (punteggio = 0 and ritirato))
 );
 

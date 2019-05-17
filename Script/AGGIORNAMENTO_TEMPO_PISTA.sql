93bd308f76f5acebeaff7f8f06ca32aca67efea9
fonzdm <33768748+fonzdm@users.noreply.github.com>
2019-05-17 22:46:47 +0200

    alcune modifiche di formattazione + issue
    aggiunta nota file di aggiornamento miglior tempo

--- Script/AGGIORNAMENTO_TEMPO_PISTA.sql
+++ Script/AGGIORNAMENTO_TEMPO_PISTA.sql
@@ -2,22 +2,21 @@ create or replace function AGGIORNAMENTO_TEMPO_PISTA() returns trigger as $$
 begin
 	-- controllare che ci siano 20 risultati
 	if( 20 = (	select count(*)
-				from risultati
-				) and exists(select min( all miglior_tempo)
-			  					  from risultati r join piste p on (r.sede_pista=p.sede_pista and r.nome_pista=p.nome_pista)
-								  where miglior_tempo <= giro_veloce)
-	   )
-								  
+				from risultati) 
+			and 
+		exists (select min ( all miglior_tempo)
+			  	from risultati r join piste p on (r.sede_pista=p.sede_pista and r.nome_pista=p.nome_pista)
+				where miglior_tempo <= giro_veloce)
+	   )						  
 	then
-			  update piste
-			  set giro_veloce =  (select min( all miglior_tempo)
+		update piste
+		set giro_veloce =  (select min( all miglior_tempo)
 			  					  from risultati r join piste p on (r.sede_pista=p.sede_pista and r.nome_pista=p.nome_pista)
 								  where miglior_tempo <= giro_veloce)
 			  where piste.sede_pista = all (select sede_pista
 									   	from risultati) and piste.nome_pista = all(select nome_pista
 									   											    from risultati);
-
-			  
+			-- Si potrebbe trovare un modo per non rifare la query? 
 	end if;
 			   
 return NULL;
--- Script/Viste.sql
+++ Script/Viste.sql
@@ -1,40 +1,37 @@
 -- Script per la generazione e l'aggiornamento delle viste principali
 
 
-create or replace function CREAZIONE_VIEW_CLASSIFICA_PILOTI() returns trigger as $$
-begin
+-- create or replace function CREAZIONE_VIEW_CLASSIFICA_PILOTI() returns trigger as $$
+-- begin
 
 	create view CLASSIFICA_PILOTI(numero_campionato, codice_pilota, punteggio) as 
 		select numero_campionato, codice_pilota, sum(punteggio)
 		from risultati
-		where numero_campionato = NEW.numero_campionato
-		group by codice_pilota
+		group by numero_campionato, codice_pilota
 		order by sum(punteggio) desc;
 	
-return null;
-end $$ language plpgsql;
+-- return null;
+-- end $$ language plpgsql;
 
-create or replace function CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI() returns trigger as $$
-begin
+-- create or replace function CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI() returns trigger as $$
+-- begin
 
-create view CLASSIFICA_COSTRUTTORI (numero_campionato, nome_scuderia, punteggio) as
+	create view CLASSIFICA_COSTRUTTORI (numero_campionato, nome_scuderia, punteggio) as
+		select A.numero_campionato, nome_scuderia, sum(C.punteggio)
+		from CLASSIFICA_PILOTI as C join AFFERENZA_PILOTI as A on (C.codice_pilota = A.codice_pilota)
+		group by  C.numero_campionato, nome_scuderia
+		order by sum(C.punteggio) desc;
+		
+-- return null;
+-- end $$ language plpgsql;
 
-	select A.numero_campionato, nome_scuderia, sum(C.punteggio)
-	from CLASSIFICA_PILOTI as C join AFFERENZA_PILOTI as A on (C.codice_pilota = A.codice_pilota)
-	where A.numero_campionato = NEW.numero_campionato
-	group by nome_scuderia
-	order by sum(C.punteggio) desc;
-	
-return null;
-end $$ language plpgsql;
+-- create trigger CREAZIONE_VIEW_CLASSIFICA_PILOTI 
+-- after insert on campionati
+-- for each statement
+-- execute procedure CREAZIONE_VIEW_CLASSIFICA_PILOTI();
 
-create trigger CREAZIONE_VIEW_CLASSIFICA_PILOTI 
-after insert on campionati
-for each statement
-execute procedure CREAZIONE_VIEW_CLASSIFICA_PILOTI();
-
-create trigger CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI
-after insert on campionati
-for each statement
-execute procedure CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI();
+-- create trigger CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI
+-- after insert on campionati
+-- for each statement
+-- execute procedure CREAZIONE_VIEW_CLASSIFICA_COSTRUTTORI();
 

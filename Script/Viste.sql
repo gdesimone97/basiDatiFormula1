2198df043222c3fbfc5f6a6b25cc9d816001df51
fonzdm <33768748+fonzdm@users.noreply.github.com>
2019-05-20 19:56:59 +0200

    Update Viste.sql
    indentato bene :joy:

--- Script/Viste.sql
+++ Script/Viste.sql
@@ -2,12 +2,12 @@
 -- le due viste "attuali" vengono aggiornate ad ogni inserimento su risultati_attuali
 
 create view CLASSIFICA_PILOTI_ATTUALE(codice_pilota, punteggio) as 
-		select codice_pilota, P.nome_pilota, P.cognome_pilota, sum(punteggio) as punti
-		from risultati_attuali as A join piloti as P on (A.codice_pilota = P.codice_pilota)
-		where numero_campionato = ( select max(numero_campionato)
-									from campionati )
-		group by codice_pilota
-		order by punti desc;
+	select codice_pilota, P.nome_pilota, P.cognome_pilota, sum(punteggio) as punti
+	from risultati_attuali as A join piloti as P on (A.codice_pilota = P.codice_pilota)
+	where numero_campionato = ( select max(numero_campionato)
+								from campionati )
+	group by codice_pilota
+	order by punti desc;
 
 create view CLASSIFICA_COSTRUTTORI_ATTUALE (nome_scuderia, punteggio) as
 	select nome_scuderia, sum(C.punteggio) as punti
@@ -21,10 +21,10 @@ create view CLASSIFICA_COSTRUTTORI_ATTUALE (nome_scuderia, punteggio) as
 -- (ovvero dopo che il trigger sposta i 420 risultati_attuali in risultati_passati) (operazione costosa ma fatta 1 V/A)
 
 create view CLASSIFICHE_PILOTI_PASSATI(numero_campionato, codice_pilota, punteggio) as 
-		select numero_campionato, codice_pilota, P.nome_pilota, P.cognome_pilota, sum(punteggio) as punti
-		from risultati_passati as A join piloti as P on (A.codice_pilota = P.codice_pilota)
-		group by numero_campionato, codice_pilota
-		order by punti desc;
+	select numero_campionato, codice_pilota, P.nome_pilota, P.cognome_pilota, sum(punteggio) as punti
+	from risultati_passati as A join piloti as P on (A.codice_pilota = P.codice_pilota)
+	group by numero_campionato, codice_pilota
+	order by punti desc;
 	
 create view CLASSIFICHE_COSTRUTTORI_PASSATE (numero_campionato, nome_scuderia, punteggio) as
 	select numero_campionato, nome_scuderia, sum(C.punteggio) as punti

0d0da859d4d6637f79dbad85242c6774838cfca5
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-05-23 15:00:28 +0200

    Merge origin/master
    Conflicts:
    	oop2018.secondaprova.gruppo07/src/oop2018/secondaprova/gruppo07/MainFrame.java

--- Java/Formula1/src/formula1/Query.java
+++ Java/Formula1/src/formula1/Query.java
@@ -24,18 +24,15 @@ public class Query {
     private static final String pass = "password";
     private static Connection conn;
 
-    private static PreparedStatement pstSelezionaPilota=null;
+    private static PreparedStatement pstSelezionaPilota = null;
 
     public Query() {
 
     }
 
-    static {
-        try {
-            conn = DriverManager.getConnection(url, user, pass);
-        } catch (SQLException ex) {
-            ;
-        }
+    public static void InitConnection() throws SQLException {
+        conn = DriverManager.getConnection(url, user, pass);
+
     }
 
     public static ResultSet getClassificaPilotiAttuale() throws SQLException {
@@ -93,7 +90,7 @@ public class Query {
         if (x >= 0 && x <= 19) {
             q += "where codice_pilota = (select codice_pilota form CLASSIFICA_PILOTA_ATTUALE offset ? limit 1)";
         } else if (x >= 20 && x <= 29) {
-            x=x-20;
+            x = x - 20;
             q += "where nome_scuderia = (select nome_scuderia form CLASSIFICA_COSTRUTTORI_ATTUALE offset ? limit 1)";
         } else {
             System.out.println("Parametro errato nel metodo: \"selezionaAfferenza\" ");

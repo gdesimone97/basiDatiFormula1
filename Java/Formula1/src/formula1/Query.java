b1f00db71520d2649d7123518cc3b88f8f65ea00
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-05-23 14:49:22 +0200

    Merge origin/master
    Conflicts:
    	oop2018.secondaprova.gruppo07/src/oop2018/secondaprova/gruppo07/MainFrame.java

--- Java/Formula1/src/formula1/Query.java
+++ Java/Formula1/src/formula1/Query.java
@@ -93,7 +93,7 @@ public class Query {
         if (x >= 0 && x <= 19) {
             q += "where codice_pilota = (select codice_pilota form CLASSIFICA_PILOTA_ATTUALE offset ? limit 1)";
         } else if (x >= 20 && x <= 29) {
-            q += "where codice_pilota = (select nome_scuderia form CLASSIFICA_COSTRUTTORI_ATTUALE offset ? limit 1)";
+            q += "where nome_scuderia = (select nome_scuderia form CLASSIFICA_COSTRUTTORI_ATTUALE offset ? limit 1)";
         } else {
             System.out.println("Parametro errato nel metodo: \"selezionaAfferenza\" ");
         }

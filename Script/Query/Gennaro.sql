0d3fcc00ea9211bb610116afc38529afc87f6afb
GiackV <1997gennaro@gmail.com>
2019-05-23 10:22:59 +0200

    Update Gennaro.sql

--- Script/Query/Gennaro.sql
+++ Script/Query/Gennaro.sql
@@ -2,7 +2,7 @@
 
 select r.numero_campionato, r.sede_pista, r.nome_pista, p.nome_scuderia
 from risultati_attuali r join afferenza_piloti p 
-on r.codice_pilota = p.codice_pilota AND r.numero_campionato = p.numero_campionato
+	on r.codice_pilota = p.codice_pilota AND r.numero_campionato = p.numero_campionato
 where (r.punteggio = 26 or r.punteggio = 25 or r.punteggio = 18 or r.punteggio = 19)
 group by r.numero_campionato, r.sede_pista, r.nome_pista, p.nome_scuderia
 having count(*)=2

d8690cc0fe39bcc7c97e9148973cfdabf0413e5d
GiackV <1997gennaro@gmail.com>
2019-05-26 23:07:33 +0200

    Update Comandi DCL.sql

--- Script/Comandi DCL.sql
+++ Script/Comandi DCL.sql
@@ -2,8 +2,8 @@
 revoke create on schema public from public;
 drop role amministratore;
 drop role utente_generico;
-create role amministratore superuser login;
-create role utente_generico login;
+create role amministratore superuser login password 'abc123';
+create role utente_generico login password 'abc123';
 
 grant select on all tables in schema public to amministratore;
 grant select on all tables in schema public to utente_generico;

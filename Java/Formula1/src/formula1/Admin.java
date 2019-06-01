2b35a506d2f8bda54f6e113c14bbb60ebe20eb10
GiackV <1997gennaro@gmail.com>
2019-06-01 15:16:56 +0200

    aggiustamenti

--- Java/Formula1/src/formula1/Admin.java
+++ Java/Formula1/src/formula1/Admin.java
@@ -20,7 +20,7 @@ import java.util.Scanner;
  */
 public class Admin {
 
-    private static final String USER_ADMIN = "postgres";
+    private static final String USER_ADMIN = "amministratore";
     private static final String PASS_ADMIN = "abc123";
     private static final String URL = "jdbc:postgresql://localhost/prova";
     private static Connection conn;

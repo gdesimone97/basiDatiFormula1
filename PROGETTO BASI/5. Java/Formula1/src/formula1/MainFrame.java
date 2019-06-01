8cb397e9b12f74b2b6a8634492415a2a7c35ad30
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-06-01 14:18:20 +0200

    Documentazione

--- PROGETTO BASI/5. Java/Formula1/src/formula1/MainFrame.java
+++ PROGETTO BASI/5. Java/Formula1/src/formula1/MainFrame.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package formula1;
 
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -29,7 +24,7 @@ import javax.swing.table.TableColumnModel;
 
 /**
  *
- * @author 1997g
+ * @author gruppo05
  */
 public class MainFrame extends javax.swing.JFrame {
 
--- PROGETTO BASI/5. Java/Formula1/src/formula1/TempoGiro.java
+++ PROGETTO BASI/5. Java/Formula1/src/formula1/TempoGiro.java
@@ -7,13 +7,22 @@ package formula1;
 
 /**
  *
- * @author desio
+ * @author gruppo05
  */
 public class TempoGiro {
 
     private TempoGiro() {
     }
 
+    /**
+     * Prende come parametri d' ingresso minuti,secondi, millisecondi e
+     * restiuisce il valore espresso in millisecondi.
+     *
+     * @param minuti
+     * @param secondi
+     * @param mSecondi
+     * @return Restituisce il valore temporale espresso in millisecondi
+     */
     public static int generaGiro(int minuti, int secondi, int mSecondi) {
         return (minuti * 60 + secondi) * 1000 + mSecondi;
     }

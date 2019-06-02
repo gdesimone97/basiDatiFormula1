63b0b599f4e766328f5ebc3ad46523afe9d3c019
fonzdm <33768748+fonzdm@users.noreply.github.com>
2019-06-02 19:48:17 +0200

    Update Tempi migliori per pilota.sql

--- PROGETTO BASI/4. SQL/Query/Tempi migliori per pilota.sql
+++ PROGETTO BASI/4. SQL/Query/Tempi migliori per pilota.sql
@@ -1,11 +1,10 @@
 --	Per ogni  pilota, il suo tempo migliore per ogni pista, indicando se è o meno il record della relativa pista (100 v/s)
 
 /*	
-	Creazione di una vista temporanea per avere tutti i risultati, attuali e passati. 
+	Creazione di una vista per avere tutti i risultati, attuali e passati. 
 	Di questi si porta solo la chiave composta e il miglior_tempo
 */
 	drop view if exists tmp_RISULTATI cascade;
-
 	 create view tmp_RISULTATI(nome_pista, sede_pista, codice_pilota, miglior_tempo, numero_campionato) as
 		select RA.nome_pista, RA.sede_pista, RA.codice_pilota, RA.miglior_tempo, (select max(numero_campionato) from campionati) as numero_campionato
 		from risultati_attuali as RA	
@@ -22,6 +21,7 @@
 */
 --	Della vista precedente, si raggruppa per pilota e per pista (nome e sede) selezionando solo il record su ogni pista
 --	si usa il costrutto CASE-WHEN-THEN per settare il campo RECORD
+
 	select codice_pilota, tR.nome_pista, tR.sede_pista, min(miglior_tempo) as RECORD_PERSONALE, 
 			CASE WHEN min(miglior_tempo) = P.giro_veloce THEN 'RECORD' ELSE 'NO RECORD' END as RECORD
 	from tmp_RISULTATI as tR, piste as P
